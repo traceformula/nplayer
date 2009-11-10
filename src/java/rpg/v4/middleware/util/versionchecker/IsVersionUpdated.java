de15ce58735e863386de18b116fbc67f649698de
fjelds <fjelds@99184ba4-cdba-11de-8d2e-8bf2cb4fc7ae>
2009-11-10 12:22:14 +0000

    Checks web for new version

---
+++ src/java/rpg/v4/middleware/util/versionchecker/IsVersionUpdated.java
@@ -0,0 +1,96 @@
+package rpg.v4.middleware.util.versionchecker;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: fjelds
+ * Date: Nov 10, 2009
+ * Time: 11:33:14 AM
+ *
+ * Checks with google code if version is updated
+ */
+
+import rpg.v4.client.gui.GameMasterFrame;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class IsVersionUpdated
+{
+    private static final Logger logger = Logger.getLogger(IsVersionUpdated.class);
+    private static final String urlString = "http://code.google.com/p/nplayer/downloads/list";
+    private static final Pattern pattern = Pattern.compile("nPlayer v4\\.[0-9]+\\.[0-9]+ ");
+
+    public static void main(String[] args)
+    {
+        ClassLoader loader = GameMasterFrame.class.getClassLoader();
+
+        String log4jPropertiesFilename = "/rpg/log4j.properties";
+
+        if (loader != null)
+        {
+            URL propertiesFile = IsVersionUpdated.class.getResource(log4jPropertiesFilename);
+            PropertyConfigurator.configure(propertiesFile);
+        }
+        
+        checkVersion();
+    }
+
+    public static boolean checkVersion()
+    {
+        URL u;
+        String thisLine = null;
+
+        //Open the URL for reading
+        try
+        {
+            u = new URL(urlString);
+            try
+            {
+                InputStream theHTML = u.openStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(theHTML));
+                while ((thisLine = reader.readLine()) != null)
+                {
+                    Matcher matcher = pattern.matcher(thisLine);
+                    if (matcher.find())
+                    {
+                        logger.info("Found: " + matcher.group());
+                        return isVersionDifferent(matcher.group());
+                    }
+                }
+            } // end try
+            catch (Exception e)
+            {
+                System.err.println(e);
+            }
+        } // end try
+        catch (MalformedURLException e)
+        {
+            System.err.println(urlString + " is not a parseable URL");
+            System.err.println(e);
+        }
+
+        return false;
+    }
+
+    private static boolean isVersionDifferent(String nplayerText)
+    {
+        String versionString = nplayerText.replaceAll("nPlayer v", "").replaceAll(" ", "").replaceAll("\\.", "");
+        String currentVersionString = GameMasterFrame.VERSION.replaceAll("\\.", "");
+
+        Integer webVersion = Integer.parseInt(versionString);
+        Integer currentVersion = Integer.parseInt(currentVersionString);
+        boolean webVersionIsNewer = webVersion > currentVersion;
+
+        logger.info("Is currentVersion (" + currentVersion + ") different than web version ("
+                + webVersion + "): " + webVersionIsNewer);
+
+        return webVersionIsNewer;
+    }
+
+}
