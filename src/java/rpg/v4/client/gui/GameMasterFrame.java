43235bc6c8a6eaa3dbf32f54f8875f2a9cf71132
fjelds <fjelds@99184ba4-cdba-11de-8d2e-8bf2cb4fc7ae>
2010-02-03 21:54:03 +0000

    version number increase

--- src/java/rpg/v4/client/gui/GameMasterFrame.java
+++ src/java/rpg/v4/client/gui/GameMasterFrame.java
@@ -30,7 +30,7 @@ public class GameMasterFrame extends JFrame
     public static JFrame frame;
     private static final int WIDTH = 940, HEIGHT = 700;
     private static final String APPLICATION_TITLE = "nPlayer   4";
-    public static final String VERSION = "4.1.2"; 
+    public static final String VERSION = "4.1.3"; 
 
     public GameMasterFrame() throws HeadlessException
     {
