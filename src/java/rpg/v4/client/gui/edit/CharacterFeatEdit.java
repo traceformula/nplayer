14bef2930cb9e1d4b2ebed3ded092a03cba36ebf
fjelds <fjelds@99184ba4-cdba-11de-8d2e-8bf2cb4fc7ae>
2010-02-03 21:01:56 +0000

    Fleshing out feat GUI.

--- src/java/rpg/v4/client/gui/edit/CharacterFeatEdit.java
+++ src/java/rpg/v4/client/gui/edit/CharacterFeatEdit.java
@@ -2,33 +2,72 @@ package rpg.v4.client.gui.edit;
 
 import rpg.swingx.JContentRenderingPanel;
 import rpg.swingx.JTransparentPanel;
-import rpg.swingx.JTransparentSplitPane;
-import rpg.v4.client.gui.edit.inventory.ItemListPanel;
-import rpg.v4.client.gui.edit.inventory.MerchantPanel;
-import rpg.v4.client.gui.edit.inventory.SlotsViewPanel;
 import rpg.v4.client.provider.impl.CharacterProvider;
+import rpg.v4.client.gui.util.factories.LabelFactory;
+import rpg.v4.client.gui.edit.feats.FeatPanel;
+import rpg.v4.client.proxy.ClientProxyKit;
 import rpg.v4.server.entity.Entity;
 import rpg.v4.server.state.State;
+import rpg.v4.middleware.util.collection.ObservableArrayList;
 
 import javax.swing.*;
-import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Map;
+import java.awt.*;
 
 /**
  * Displays feats of the active character available.
  */
 public class CharacterFeatEdit extends JContentRenderingPanel implements Observer
 {
-    private JPanel characterInventoryPanel;
     private Entity activeEntity;
+    private JPanel featListings;
+
 
     public CharacterFeatEdit()
     {
-        characterInventoryPanel = new JTransparentPanel();
+        JLabel label = LabelFactory.createHeaderLargeLabel("Feats");
+        add(label, BorderLayout.NORTH);
+
+        featListings = new JTransparentPanel();
+        featListings.setLayout(new FlowLayout(FlowLayout.LEFT));
+        featListings.setPreferredSize(new Dimension(300,300));
+        add(featListings, BorderLayout.CENTER);
+
+        ObservableArrayList<String> featList = ClientProxyKit.CLIENT_PROXY.getAvailableFeats();
+        featList.addObserver(this);
 
         CharacterProvider.provider.addObserver(this);
         CharacterSidePanel.addPanelAsInvoker(this);
+
+        updateFeats();
+    }
+
+    private void updateFeats()
+    {
+        featListings.removeAll();
+        featListings.setLayout(new FlowLayout(FlowLayout.LEFT));
+        featListings.setPreferredSize(new Dimension(300,300));
+        ObservableArrayList<String> featList = ClientProxyKit.CLIENT_PROXY.getAvailableFeats();
+        for (String featName : featList)
+        {
+            FeatPanel featPanel = new FeatPanel(featName);
+            featListings.add(featPanel);
+        }
+
+        this.revalidate();
+        this.repaint();
+    }
+
+    private void updateFeatSelection()
+    {
+        if (null == activeEntity)
+        {
+            return;
+        }
+
+        
     }
 
     public void update(Observable o, Object arg)
@@ -39,6 +78,10 @@ public class CharacterFeatEdit extends JContentRenderingPanel implements Observe
         } else if (o instanceof State)
         {
             // Update which feats are available
+        } else if (o.equals(ClientProxyKit.CLIENT_PROXY.getAvailableFeats()))
+        {
+            // Feat list has been updated.
+            updateFeats();
         }
     }
 
