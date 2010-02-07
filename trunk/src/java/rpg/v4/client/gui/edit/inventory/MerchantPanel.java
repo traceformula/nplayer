0373ee54570ec690ce5a8b5cbb999a07b36c2ff5
fjelds <fjelds@99184ba4-cdba-11de-8d2e-8bf2cb4fc7ae>
2010-02-07 11:25:58 +0000

    Cheap seperation of items/armor

--- trunk/src/java/rpg/v4/client/gui/edit/inventory/MerchantPanel.java
+++ trunk/src/java/rpg/v4/client/gui/edit/inventory/MerchantPanel.java
@@ -14,6 +14,8 @@ import java.awt.*;
 import java.util.*;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 /**
  * Lists all items available in the application and allows a user to add it to the active
  * entity's inventory. If any new items become available through the forges then these items
@@ -21,11 +23,14 @@ import java.util.List;
  */
 public class MerchantPanel extends JBorderLayoutPanel implements Observer
 {
+    private static final Logger logger = Logger.getLogger(MerchantPanel.class);
+
     private Map<String, Object> weaponRowMap;
     private Map<String, Object> armorRowMap;
-    private Box weaponBox, armorBox;
+    private Map<String, Object> itemsRowMap;
+    private Box weaponBox, armorBox, itemsBox;
     private ObservableArrayList<String> weaponList;
-    private ObservableArrayList<String> armorList;
+    private ObservableArrayList<String> nonWeaponsItemList;
 
     public MerchantPanel()
     {
@@ -34,8 +39,8 @@ public class MerchantPanel extends JBorderLayoutPanel implements Observer
         weaponList = ClientProxyKit.CLIENT_PROXY.getAvailableWeapons();
         weaponList.addObserver(this);
 
-        armorList = ClientProxyKit.CLIENT_PROXY.getAvailableItems();
-        armorList.addObserver(this);
+        nonWeaponsItemList = ClientProxyKit.CLIENT_PROXY.getAvailableItems();
+        nonWeaponsItemList.addObserver(this);
 
         JLabel label = LabelFactory.createHeaderLargeLabel("Merchant");
         add(label, BorderLayout.NORTH);
@@ -43,19 +48,24 @@ public class MerchantPanel extends JBorderLayoutPanel implements Observer
         JDarkTabbedPane tabbedPane = new JDarkTabbedPane();
         weaponBox = Box.createVerticalBox();
         armorBox = Box.createVerticalBox();
+        itemsBox = Box.createVerticalBox();
 
         addTab("Weapons", weaponBox, tabbedPane);
-        addTab("Items", armorBox, tabbedPane);
+        addTab("Armor", armorBox, tabbedPane);
+        addTab("Items", itemsBox, tabbedPane);
         this.add(tabbedPane, BorderLayout.CENTER);
 
         weaponRowMap = new HashMap<String, Object>();
         manageChange(weaponList, weaponRowMap, weaponBox);
 
         armorRowMap = new HashMap<String, Object>();
-        manageChange(armorList, armorRowMap, armorBox);
+        manageChange(nonWeaponsItemList, armorRowMap, armorBox);
+
+        itemsRowMap = new HashMap<String, Object>();
+        manageChange(nonWeaponsItemList, itemsRowMap, itemsBox);
 
         setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 5));
-        setPreferredSize(new Dimension(50,50));
+        setPreferredSize(new Dimension(50, 50));
         setBackground(ColorConstants.SIDEBAR_BG);
         setBorder(BorderFactory.createMatteBorder(0, 1, 0, 0, Color.BLACK));
     }
@@ -80,30 +90,41 @@ public class MerchantPanel extends JBorderLayoutPanel implements Observer
         {
             if (o == weaponList)
                 manageChange(arg, weaponRowMap, weaponBox);
-            else if (o == armorList)
+            else if (o == nonWeaponsItemList)
                 manageChange(arg, armorRowMap, armorBox);
         }
     }
 
     private void manageChange(Object arg, Map map, Box box)
     {
+        boolean isWeapon = map == weaponRowMap;
+        boolean isArmor = map == armorRowMap;
+        boolean isItem = map == itemsRowMap;
+
         for (String name : (List<String>) arg)
         {
             // Found new item that has been added so add a merchant row.
             if (!map.containsKey(name))
             {
+                boolean isArmorType = false;
                 V4ItemCapsule itemCapsule = null;
-                if (map == weaponRowMap)
+                if (isWeapon)
                 {
                     itemCapsule = ClientProxyKit.CLIENT_PROXY.getWeaponItemCapsule(name);
-                }
-                else if (map == armorRowMap)
+                } else
                 {
                     itemCapsule = ClientProxyKit.CLIENT_PROXY.getArmorItemCapsule(name);
+                    isArmorType = "Armor".equals(itemCapsule.getV4Item().getType());
                 }
-                MerchantItemRow row = new MerchantItemRow(itemCapsule);
-                box.add(row);
-                map.put(name, row);
+
+                boolean toAdd = isWeapon || (isArmor && isArmorType) || (isItem && !isArmorType);
+
+                if (toAdd)
+                {
+                    MerchantItemRow row = new MerchantItemRow(itemCapsule);
+                    box.add(row);
+                    map.put(name, row);
+                }
             }
 
             box.revalidate();
